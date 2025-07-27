22681ad50d4858f42eb46ad51937b81274b2083a
rhernandezbas <ronald.hernandezba@gmail.com>
2025-07-27 04:01:26 -0300

    feature: agregando nuevo subnivel

--- src/features/estudiantes/pages/EstudianteForm.jsx
+++ src/features/estudiantes/pages/EstudianteForm.jsx
@@ -311,6 +311,7 @@ const EstudianteForm = () => {
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
+                <option value="4">4</option>
               </select>
             </div>
           </div>
